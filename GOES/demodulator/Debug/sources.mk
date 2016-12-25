e5e33646fbbcd09e3b2a2bbbbd44908f1db2b9c5
Lucas Teske <lucas@teske.net.br>
2016-12-25 08:33:44 -0200

    Added stand-alone demodulator prototype.
    *	Added FFT Windows from GNU Radio
    *	Fixed some bugs on Filters
    *	Added base stand-alone demodulator prototype

--- GOES/demodulator/Debug/sources.mk
+++ GOES/demodulator/Debug/sources.mk
@@ -12,9 +12,9 @@ CPP_SRCS :=
 C_SRCS := 
 O_SRCS := 
 S_UPPER_SRCS := 
-LIBRARIES := 
 CC_DEPS := 
 C++_DEPS := 
+EXECUTABLES := 
 C_UPPER_DEPS := 
 CXX_DEPS := 
 OBJS := 
@@ -23,7 +23,5 @@ C_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-. \
 src \
-src/dsp \
 
